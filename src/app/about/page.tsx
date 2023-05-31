a42a039f76e7be874d53a4d644190fbd7ae41233
Aidos Smagulovm <aidos49@yahoo.com>
2023-06-01 02:02:12 +0600

    feat: add basic about page

---
+++ src/app/about/page.tsx
@@ -0,0 +1,8 @@
+export default function About() {
+  return (
+    <div>
+      <h2>Источники:</h2>
+      <p><b>Геоданные Казахстана: </b><a href="https://github.com/open-data-kazakhstan/geo-boundaries-kz">Geo data for Kazakhstan</a> — <a href="https://www.opendatagov.kz/">Open Data Kz</a></p>
+    </div>
+  );
+}
