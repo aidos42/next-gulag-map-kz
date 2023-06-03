cc985f73557beb87e356c8966fe877628616a7dd
Aidos Smagulov <aidos49@yahoo.com>
2023-06-03 19:47:30 +0600

    fix: change div to react fragment

--- src/app/about/page.tsx
+++ src/app/about/page.tsx
@@ -1,9 +1,9 @@
 export default function About() {
   return (
-    <div>
+    <>
       <h1>О проекте</h1>
       <h2>Источники:</h2>
       <p><b>Геоданные Казахстана: </b><a href="https://github.com/open-data-kazakhstan/geo-boundaries-kz">Geo data for Kazakhstan</a> — <a href="https://www.opendatagov.kz/">Open Data Kz</a></p>
-    </div>
+    </>
   );
 }
